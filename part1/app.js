206b53fb77049cbdaf1d4206c2ad18208895d438
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 08:31:46 +0000

    [automatic commit] 2025-06-20 08:31:46 UTC 139.218.238.212

--- part1/app.js
+++ part1/app.js
@@ -173,7 +173,7 @@ app.get('/api/walkers/summary',async (req,res,next) => {
         const [summary] = await db.execute(`
             SELECT Users.username AS walker_username,COUNT(WalkRatings.rating_id) AS total_ratings,AVG(WalkRatings.average_rating) AS average_rating,(SELECT * FROM WalkRequests JOIN WalkApplications ON WalkApplications.request_id = WalkRequests.requests_id WHERE WalkApplications.status = 'completed' AND WalkRatings.walker_id = Users.user_id) AS completed_walks
             From Users
-            LEFT JOIN WalkRatings
+            LEFT JOIN WalkRatings ON WalkRatings.walker_id = 
         `);
         res.json(summary);
     }catch(error){
