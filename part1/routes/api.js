e9d3c74b8743ae246122098b7157b0cb97378f3a
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 06:06:47 +0000

    [automatic commit] 2025-06-20 06:06:47 UTC 139.218.238.212

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -28,7 +28,17 @@ router.get('/walkrequests/open',async (req,res,next) => {
 });
 
 router.get('/walkers/summary',async (req,res,next) => {
-
+    try{
+        const [summary] = await req.db.execute(`
+            SELECT request_id,Dogs.name AS dog_name,requested_time,duration_minutes,location,Users.username AS owner_username FROM WalkRequests
+            JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id
+            JOIN Users ON Users.user_id = Dogs.owner_id
+            WHERE WalkRequests.status = 'open'
+        `);
+        res.json(request);
+    }catch(error){
+        res.status(500).json({ error: 'Failed to fetch Summary' });
+    }
 });
 
 module.exports = router;
