0c78ebe0c6a91769d1f28cb0c8e0914802120845
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:50:21 +0000

    [automatic commit] 2025-06-20 05:50:21 UTC 139.218.238.212

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/dogs',async (req,res,next) => {
-    console.log("object");
     try{
         const [rows] = await req.db.execute(`
             SELECT name AS dog_name,size,Users.username AS owner_username FROM Dogs
@@ -10,8 +9,18 @@ router.get('/dogs',async (req,res,next) => {
         `);
         res.json(rows);
     }catch(error){
-        console.log(error);
-        // res.status(500).json({ error: 'Failed to fetch Dogs' });
+        res.status(500).json({ error: 'Failed to fetch Dogs' });
+    }
+});
+router.get('/dogs',async (req,res,next) => {
+    try{
+        const [rows] = await req.db.execute(`
+            SELECT name AS dog_name,size,Users.username AS owner_username FROM Dogs
+            JOIN Users ON Dogs.owner_id = Users.user_id
+        `);
+        res.json(rows);
+    }catch(error){
+        res.status(500).json({ error: 'Failed to fetch Dogs' });
     }
 });
 
