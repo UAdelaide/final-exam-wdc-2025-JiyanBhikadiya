2a8460083948075130c79cb95fabdeb78f78396c
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 06:40:00 +0000

    [automatic commit] 2025-06-20 06:40:00 UTC 139.218.238.212

--- part1/app.js
+++ part1/app.js
@@ -102,40 +102,40 @@ let db;
         `);
 
         // Insert data if table is empty
-        const [rows1] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-        if (rows1[0].count === 0) {
-            await db.execute(`
-                INSERT INTO Users(username,email,password_hash,role) VALUES
-                ('alice123','alice@example.com','hashed123','owner'),
-                ('bobwalker','bob@example.com','hashed456','walker'),
-                ('carol123','carol@example.com','hashed789','owner'),
-                ('jiyan123','jiyan@example.com','jiyan123','owner'),
-                ('jiyanwalker','jbwalk@example.com','jbwalk123','walker');
-            `);
-        }
-        // Insert data if table is empty
-        const [rows2] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
-        if (rows2[0].count === 0) {
-            await db.execute(`
-                ((SELECT user_id FROM Users WHERE username = 'alice123'),'Max','medium'),
-                ((SELECT user_id FROM Users WHERE username = 'carol123'),'Bella','small'),
-                ((SELECT user_id FROM Users WHERE username = 'alice123'),'Rocko','large'),
-                ((SELECT user_id FROM Users WHERE username = 'jiyan123'),'Dollar','large'),
-                ((SELECT user_id FROM Users WHERE username = 'jiyan123'),'Visky','medium');
-            `);
-        }
-        // Insert data if table is empty
-        const [rows3] = await db.execute('SELECT COUNT(*) AS count FROM  WalkRequests');
-        if (rows3[0].count === 0) {
-            await db.execute(`
-                INSERT INTO WalkRequests (dog_id,requested_time,duration_minutes,location,status) VALUES
-                ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),'2025-06-10 08:00:00',30,'Parklands','open'),
-                ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),'2025-06-10 09:30:00',45,'Beachside Ave','accepted'),
-                ((SELECT dog_id FROM Dogs WHERE name = 'Rocko' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),'2025-06-13 12:30:00',25,'Salisbury Park','completed'),
-                ((SELECT dog_id FROM Dogs WHERE name = 'Dollar' AND owner_id = (SELECT user_id FROM Users WHERE username = 'jiyan123')),'2025-06-15 08:30:00',55,'S8ul Gaming House','open'),
-                ((SELECT dog_id FROM Dogs WHERE name = 'Visky' AND owner_id = (SELECT user_id FROM Users WHERE username = 'jiyan123')),'2025-06-15 08:30:00',55,'S8ul Gaming House','open');
-            `);
-        }
+        // const [rows1] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+        // if (rows1[0].count === 0) {
+        //     await db.execute(`
+        //         INSERT INTO Users(username,email,password_hash,role) VALUES
+        //         ('alice123','alice@example.com','hashed123','owner'),
+        //         ('bobwalker','bob@example.com','hashed456','walker'),
+        //         ('carol123','carol@example.com','hashed789','owner'),
+        //         ('jiyan123','jiyan@example.com','jiyan123','owner'),
+        //         ('jiyanwalker','jbwalk@example.com','jbwalk123','walker');
+        //     `);
+        // }
+        // // Insert data if table is empty
+        // const [rows2] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
+        // if (rows2[0].count === 0) {
+        //     await db.execute(`
+        //         ((SELECT user_id FROM Users WHERE username = 'alice123'),'Max','medium'),
+        //         ((SELECT user_id FROM Users WHERE username = 'carol123'),'Bella','small'),
+        //         ((SELECT user_id FROM Users WHERE username = 'alice123'),'Rocko','large'),
+        //         ((SELECT user_id FROM Users WHERE username = 'jiyan123'),'Dollar','large'),
+        //         ((SELECT user_id FROM Users WHERE username = 'jiyan123'),'Visky','medium');
+        //     `);
+        // }
+        // // Insert data if table is empty
+        // const [rows3] = await db.execute('SELECT COUNT(*) AS count FROM  WalkRequests');
+        // if (rows3[0].count === 0) {
+        //     await db.execute(`
+        //         INSERT INTO WalkRequests (dog_id,requested_time,duration_minutes,location,status) VALUES
+        //         ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),'2025-06-10 08:00:00',30,'Parklands','open'),
+        //         ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),'2025-06-10 09:30:00',45,'Beachside Ave','accepted'),
+        //         ((SELECT dog_id FROM Dogs WHERE name = 'Rocko' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),'2025-06-13 12:30:00',25,'Salisbury Park','completed'),
+        //         ((SELECT dog_id FROM Dogs WHERE name = 'Dollar' AND owner_id = (SELECT user_id FROM Users WHERE username = 'jiyan123')),'2025-06-15 08:30:00',55,'S8ul Gaming House','open'),
+        //         ((SELECT dog_id FROM Dogs WHERE name = 'Visky' AND owner_id = (SELECT user_id FROM Users WHERE username = 'jiyan123')),'2025-06-15 08:30:00',55,'S8ul Gaming House','open');
+        //     `);
+        // }
     } catch (err) {
         console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
     }
