94f80ca3f9adf3a3deef6be682e1bf05d2519027
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:22:28 +0000

    [automatic commit] 2025-06-20 05:22:28 UTC 139.218.238.212

--- part1/app.js
+++ part1/app.js
@@ -106,8 +106,8 @@ let db;
             `);
         }
         // Insert data if table is empty
-        const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-        if (rows[0].count === 0) {
+        const [rows2] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+        if (rows2[0].count === 0) {
             await db.execute(`
                 INSERT INTO Users(username,email,password_hash,role) VALUES
                 ('alice123','alice@example.com','hashed123','owner'),
@@ -118,8 +118,8 @@ let db;
             `);
         }
         // Insert data if table is empty
-        const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-        if (rows[0].count === 0) {
+        const [rows3] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+        if (rows3[0].count === 0) {
             await db.execute(`
                 INSERT INTO Users(username,email,password_hash,role) VALUES
                 ('alice123','alice@example.com','hashed123','owner'),
