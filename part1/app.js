b931fc9d2d85cffdaf77d80b549c9b42d9b0c950
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 06:23:20 +0000

    [automatic commit] 2025-06-20 06:23:20 UTC 139.218.238.212

--- part1/app.js
+++ part1/app.js
@@ -141,12 +141,43 @@ let db;
     }
 })();
 
-app.use((req,res,next) => {
-    if(db){
-        req.db = db;
-        next();
-    }else{
-        res.status(500).json({ error: "Error setting up database." });
+router.get('/dogs',async (req,res,next) => {
+    try{
+        const [dogs] = await req.db.execute(`
+            SELECT name AS dog_name,size,Users.username AS owner_username FROM Dogs
+            JOIN Users ON Users.user_id = Dogs.owner_id
+        `);
+        res.json(dogs);
+    }catch(error){
+        res.status(500).json({ error: 'Failed to fetch Dogs' });
+    }
+});
+
+router.get('/walkrequests/open',async (req,res,next) => {
+    try{
+        const [request] = await req.db.execute(`
+            SELECT request_id,Dogs.name AS dog_name,requested_time,duration_minutes,location,Users.username AS owner_username FROM WalkRequests
+            JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id
+            JOIN Users ON Users.user_id = Dogs.owner_id
+            WHERE WalkRequests.status = 'open'
+        `);
+        res.json(request);
+    }catch(error){
+        res.status(500).json({ error: 'Failed to fetch Requests' });
+    }
+});
+
+router.get('/walkers/summary',async (req,res,next) => {
+    try{
+        const [summary] = await req.db.execute(`
+            SELECT request_id,Dogs.name AS dog_name,requested_time,duration_minutes,location,Users.username AS owner_username FROM WalkRequests
+            JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id
+            JOIN Users ON Users.user_id = Dogs.owner_id
+            WHERE WalkRequests.status = 'open'
+        `);
+        res.json(summary);
+    }catch(error){
+        res.status(500).json({ error: 'Failed to fetch Summary' });
     }
 });
 
